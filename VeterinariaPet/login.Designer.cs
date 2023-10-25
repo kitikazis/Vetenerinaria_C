42b9cdcd22019d5cc2cac67f11c100c85ee2c084
Yamir <luiskitikazis@gmail.com>
2023-10-25 16:43:02 -0500

    Login Cambios

--- VeterinariaPet/login.Designer.cs
+++ VeterinariaPet/login.Designer.cs
@@ -31,20 +31,20 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
             this.panel_contenedor = new System.Windows.Forms.Panel();
-            this.MenuVertical = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtbox_pass = new System.Windows.Forms.TextBox();
             this.btn_producto = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.txtbox_usuario = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MenuVertical = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.txtbox_pass = new System.Windows.Forms.TextBox();
             this.panel_contenedor.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.MenuVertical.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_contenedor
@@ -59,25 +59,25 @@
             this.panel_contenedor.Size = new System.Drawing.Size(509, 478);
             this.panel_contenedor.TabIndex = 3;
             // 
-            // MenuVertical
+            // panel1
             // 
-            this.MenuVertical.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
-            this.MenuVertical.Controls.Add(this.pictureBox1);
-            this.MenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
-            this.MenuVertical.Location = new System.Drawing.Point(0, 0);
-            this.MenuVertical.Name = "MenuVertical";
-            this.MenuVertical.Size = new System.Drawing.Size(410, 478);
-            this.MenuVertical.TabIndex = 2;
+            this.panel1.Controls.Add(this.txtbox_pass);
+            this.panel1.Controls.Add(this.btn_producto);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.txtbox_usuario);
+            this.panel1.Location = new System.Drawing.Point(77, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(351, 300);
+            this.panel1.TabIndex = 8;
             // 
-            // label1
+            // txtbox_pass
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(167, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 55);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Login";
+            this.txtbox_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_pass.Location = new System.Drawing.Point(59, 229);
+            this.txtbox_pass.Name = "txtbox_pass";
+            this.txtbox_pass.Size = new System.Drawing.Size(278, 29);
+            this.txtbox_pass.TabIndex = 7;
+            this.txtbox_pass.TextChanged += new System.EventHandler(this.txtboxx_pass_TextChanged);
             // 
             // btn_producto
             // 
@@ -96,25 +96,6 @@
             this.btn_producto.TabIndex = 2;
             this.btn_producto.Text = "Usuario";
             this.btn_producto.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaGreen;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.button2.Location = new System.Drawing.Point(278, 417);
-            this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.button2.Size = new System.Drawing.Size(208, 49);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Ingresar";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -144,6 +125,46 @@
             this.txtbox_usuario.TabIndex = 6;
             this.txtbox_usuario.TextChanged += new System.EventHandler(this.txtbox_Usuario_TextChanged);
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(278, 417);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(208, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ingresar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(181, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // MenuVertical
+            // 
+            this.MenuVertical.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.MenuVertical.Controls.Add(this.pictureBox1);
+            this.MenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuVertical.Location = new System.Drawing.Point(0, 0);
+            this.MenuVertical.Name = "MenuVertical";
+            this.MenuVertical.Size = new System.Drawing.Size(410, 478);
+            this.MenuVertical.TabIndex = 2;
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
@@ -153,32 +174,13 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.txtbox_pass);
-            this.panel1.Controls.Add(this.btn_producto);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.txtbox_usuario);
-            this.panel1.Location = new System.Drawing.Point(77, 87);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(351, 300);
-            this.panel1.TabIndex = 8;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
-            // 
-            // txtbox_pass
-            // 
-            this.txtbox_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtbox_pass.Location = new System.Drawing.Point(59, 229);
-            this.txtbox_pass.Name = "txtbox_pass";
-            this.txtbox_pass.Size = new System.Drawing.Size(278, 29);
-            this.txtbox_pass.TabIndex = 7;
-            this.txtbox_pass.TextChanged += new System.EventHandler(this.txtboxx_pass_TextChanged);
             // 
             // login
             // 
@@ -191,10 +193,10 @@
             this.Text = "login";
             this.panel_contenedor.ResumeLayout(false);
             this.panel_contenedor.PerformLayout();
-            this.MenuVertical.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.MenuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
